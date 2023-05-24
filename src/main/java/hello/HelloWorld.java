49b8e77aee4e07d74cb932912f4c9b81fba06564
ashwin <aswinkumar2697@gmail.com>
2023-05-24 14:38:00 +0530

    added a empty method in helloworld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,15 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
+	private void add(){
+
+		int a;
+		int b;
+
+
+
+
+	}
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
